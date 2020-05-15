ac9792a1767a43b242e9b475172391a7359fbc20
quijano <safiro.quijano@nokia.com>
2020-05-15 20:54:00 +0800

    setup socketio with express using raw http server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+//instead of express creating the server in the background, we manually call
+//http.createServer so that we'll have the raw http server returned
+const server = http.createServer(app);
+//because socketio takes in the raw server in its setup
+const io = socketio(server);
+
+const publicDirectory = path.join(__dirname,'../public');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(publicDirectory));
+
+server.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`);
+})
